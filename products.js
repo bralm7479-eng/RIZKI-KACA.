167b8b49be95df75fcaca23b3a63caba41f9389a
bralm7479-eng <bralm7479@gmail.com>
2026-07-23 17:37:05 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,1 @@
-const products=[{name:"Kaca Tempered",code:"KT001",price:"Rp13.000.000" image:"Kacatempered.jpeg},{name:"Cermin Bevel",code:"CB002",price:"Rp350.000"},{name:"Kaca Laminated",code:"KL003",price:"Rp650.000"}];
+const products=[{name:"Kaca Tempered",code:"KT001",price:"Rp13.000.000", image:"Kacatempered.jpeg",{name:"Cermin Bevel",code:"CB002",price:"Rp350.000"},{name:"Kaca Laminated",code:"KL003",price:"Rp650.000"}];
